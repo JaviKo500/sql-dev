2aab9ec1491316db671f4a9aa25b8d096fd3b95e
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-25 11:57:48 -0500

    insert rows data

---
+++ section-3/insert.sql
@@ -0,0 +1,20 @@
+-- unique element
+insert into
+   users (name)
+values
+   ('Isa');
+
+-- multiple element
+insert into
+   users (name)
+values
+   ('Javier'),
+   ('Pablo'),
+   ('Nube');
+
+-- insert all columns element
+insert into
+values
+   ('Javier'),
+   ('Pablo'),
+   ('Nube');
