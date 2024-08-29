c525cf6996ba649c62b4b76873d37c39d6fa2269
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-29 00:06:16 -0500

    where

---
+++ section-3/where.sql
@@ -0,0 +1,8 @@
+select * from users u where name = 'Pablo';
+
+select * from users u where name like 'P%';
+
+select * from users u where name like '_a%';
+select * from users u where name like '%p%';
+select * from users u where name like '_ablo';
+select * from users u where name like '_ablo%';
