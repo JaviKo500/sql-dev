f7786fadb9bc784e4e833e8ef8ecb0cc0fcf40e8
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-14 23:49:10 -0500

    foreign key in city

---
+++ section-05/foreign_key.sql
@@ -0,0 +1,11 @@
+select * from city c ;
+
+
+select * from country where code = 'AFG';
+alter table city 
+	add constraint fk_country_code
+	foreign key ( countrycode )
+	references country(code);
+	
+INSERT INTO country
+		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');
