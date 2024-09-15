3230773eef1809eb5d1d90439aa24ca1fe71c012
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-15 11:08:30 -0500

    update type column and set foreign key

--- section-05/foreign_key.sql
+++ section-05/foreign_key.sql
@@ -14,4 +14,14 @@ alter table "course-db".public.countrylanguage
 	add constraint fk_country_code
 	foreign key ( countrycode )
 	references country(code);
+
+-- update type column and set foreign key
+alter table country 
+alter column continent type int4
+using continent::integer;
+
+alter table country 
+	add constraint fk_continent_code
+	foreign key ( continent )
+	references continent(code);
 	
