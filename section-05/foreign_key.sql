33ec826200a3842b28c900cddff8e470b20979c9
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-14 23:58:48 -0500

    fk country_language

--- section-05/foreign_key.sql
+++ section-05/foreign_key.sql
@@ -9,3 +9,9 @@ alter table city
 	
 INSERT INTO country
 		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');
+
+alter table "course-db".public.countrylanguage 
+	add constraint fk_country_code
+	foreign key ( countrycode )
+	references country(code);
+	
