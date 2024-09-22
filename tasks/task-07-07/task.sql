5322deb93d49fd38ce8deec37c564a17d3ed5536
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-22 09:27:15 -0500

    task inner joins

---
+++ tasks/task-07-07/task.sql
@@ -0,0 +1,21 @@
+-- Count Union - Task
+-- Total |  Continent
+-- 5	  | Antarctica
+-- 28	  | Oceania
+-- 46	  | Europe
+-- 51	  | America
+-- 51	  | Asia
+-- 58	  | Africa
+
+(select count(*) as Total, b."name" as Continent from country a
+	inner join continent b
+	on a.continent = b.code
+	where b.name not like '%America%'
+	group by b.name
+	)
+union
+(select count(*) as Total, 'America' as Continent  from country a
+	inner join continent b
+	on a.continent  = b.code 
+	where b.name like '%America')
+order by Total asc;
