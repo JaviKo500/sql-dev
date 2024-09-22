9be76d24d558f16a7467af5eb25cd997e6f3813a
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-22 09:00:06 -0500

    union joins and order by

--- section-07/rigthtOuterJoin.sql
+++ section-07/rigthtOuterJoin.sql
@@ -2,3 +2,23 @@ select a.name, a.continent, b.name from country a
 	right join continent b
 	on a.continent = b.code 
 	where a.continent is null;
+
+    select count(*), b."name" from country a
+	inner join continent b
+	on a.continent  = b.code
+	group by b.name
+	order  by count(*) asc;
+	
+	
+(select count(*) as total, b."name" from country a
+	inner join continent b
+	on a.continent  = b.code
+	group by b.name)
+union
+(select 0 as total, b."name" from country a
+	right outer join continent b
+	on a.continent  = b.code
+	where a.continent is null
+	group by b.name
+)
+order by total asc;
