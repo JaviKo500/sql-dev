e34c55142fd8dc1da3dff98a10ec5744c104b3f2
JaViKo500 <javikogutierrez64@gmail.com>
2024-09-26 23:08:19 -0500

    filter date

--- section-8/dateFunctions.sql
+++ section-8/dateFunctions.sql
@@ -9,3 +9,19 @@ select now(),
 	date_part( 'months', now() ) as months,
 	date_part( 'years', now() ) as years
 	; 
+
+select * from employees e 
+	where hire_date > '1998-02-05'
+	order by hire_date;
+select * from employees e 
+	where hire_date > date('1998-02-05')
+	order by hire_date;
+
+
+select max(hire_date) as more_new,
+	min(hire_date) as firs_employe
+	from employees e;
+
+select * from employees e
+	where  hire_date  between '1990-01-01' and '2000-01-01';
+
