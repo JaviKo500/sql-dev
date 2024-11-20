b4ca02db5bfaf2534399782c5cae32e2ae337bd9
JaViKo500 <javikogutierrez64@gmail.com>
2024-11-19 22:47:16 -0500

    materialized view

---
+++ section-13/views_materialized.sql
@@ -0,0 +1,17 @@
+create materialized view comments_per_week_mat as
+select date_trunc('week', p.created_at) as weeks, 
+	count( distinct p.post_id ) as number_of_posts,
+	sum(c.counter) as total_claps
+	from posts p
+	inner join claps c on c.post_id  = p.post_id 
+	group by weeks 
+	order by weeks desc;
+
+
+select * from comments_per_week cpw ;
+
+select * from comments_per_week_mat cpwm ;
+
+refresh materialized view comments_per_week_mat ;
+
+select * from posts p where p.post_id =1;
