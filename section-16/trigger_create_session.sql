d624fab5d0e1e35670e0138ab8cc801ffbc2f90c
JaViKo500 <javikogutierrez64@gmail.com>
2024-11-27 23:10:40 -0500

    validate data trigger with WHEN

--- section-16/trigger_create_session.sql
+++ section-16/trigger_create_session.sql
@@ -14,3 +14,22 @@ begin
 end;
 $$ language plpgsql;
 
+
+
+call user_login('javiko.500', '123456');
+
+create or replace trigger create_session_trigger 
+	after update on "user"
+	for each row
+	WHEN (OLD.last_login IS DISTINCT FROM NEW.last_login)
+	execute function create_session_log();
+
+create or replace function create_session_log()
+returns trigger 
+as $$
+begin
+	insert into "session" (user_id, last_login) values( new.id, now() );
+	return new; 
+end;
+$$ language plpgsql;
+
